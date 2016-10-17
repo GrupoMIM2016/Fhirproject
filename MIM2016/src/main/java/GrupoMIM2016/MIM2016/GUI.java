e36d01a9cd733d6981490253fa07e3c80e800955
Daniel <Daniel@192.168.2.110>
2016-10-17 12:07:04 -0300

    + GUI v1.0

---
+++ MIM2016/src/main/java/GrupoMIM2016/MIM2016/GUI.java
@@ -0,0 +1,25 @@
+package GrupoMIM2016.MIM2016;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUI extends Application{
+		public static void main(String[] args) {
+			launch(args);
+		}
+		
+		@Override
+		public void start(Stage stage) throws Exception {
+			
+			Parent root = FXMLLoader.load(getClass().getResource("Fhir.fxml"));
+			
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+			stage.setScene(scene);
+			stage.setTitle("Culture Club");
+			stage.show();
+		}
+	}
