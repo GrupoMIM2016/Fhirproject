873165ccb6216fba6dd7e492ecb37fa304bd4fdf
samuel <scvaldes22@gmail.com>
2016-10-22 17:08:56 -0300

    jkgjkh

--- MIM2016/src/main/java/GrupoMIM2016/MIM2016/MainController.java
+++ MIM2016/src/main/java/GrupoMIM2016/MIM2016/MainController.java
@@ -1,128 +1,76 @@
 package GrupoMIM2016.MIM2016;
 
-import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
+import java.util.Date;
+import java.util.Locale;
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
-public class MainController implements Initializable {
-	
-	private String selectedIdType;
-	private String selectedExamDate;
-	private String selectedStatus;
-	private String selectedSampleType;
-	private String selectedResult;
-	private String selectedTestId;
-	private String selectedPatientIdentifier;
-	private String selectedComments;
-	
-	public static final LocalDate NOW_LOCAL_DATE (){
-    String date = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    LocalDate localDate = LocalDate.parse(date, formatter);
-    return localDate;
-}
-	
-	//Seleccionar la fecha de toma de muestra
-	@FXML
-	public DatePicker examDate;
-	
-	//Seleccionar el tipo de identificación
-	@FXML 
-	public ComboBox<String> IdType;
-	ObservableList<String> list = FXCollections.observableArrayList("RUT", "ACME", "Social Number");
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		IdType.setItems(list);
-		examDate.setValue(NOW_LOCAL_DATE());
-	}
-	
-	//Seleccionar el status de la muestra
-	@FXML public RadioButton FinalRdbtn;
-	@FXML public RadioButton PreliminaryRdbtn;
-	@FXML public RadioButton RegisteredRdbtn;
-	
-	//Escribir textos
-	@FXML public TextField testIdentifier;
-	@FXML public TextField patientIdentifier;
-	@FXML public TextField comments;
-	
-	public void StatusListen(ActionEvent event){
-		this.selectedStatus = "";
-		if (FinalRdbtn.isSelected()){
-			selectedStatus += FinalRdbtn.getText();
-		}
-		if (PreliminaryRdbtn.isSelected()){
-			selectedStatus += PreliminaryRdbtn.getText();
-		}
-		if (RegisteredRdbtn.isSelected()){
-			selectedStatus += RegisteredRdbtn.getText();
-		}
-	}
-	
-//Seleccionar el tipo de muestra
-	@FXML public RadioButton BloodRdbtn;
-	@FXML public RadioButton UrineRdbtn;
-	
-	public void TypeListen(ActionEvent event){
-		this.selectedSampleType = "";
-		if (BloodRdbtn.isSelected()){
-			selectedSampleType += BloodRdbtn.getText();
-		}
-		if (UrineRdbtn.isSelected()){
-			selectedSampleType += UrineRdbtn.getText();
-		}
-	}
-	
-//Seleccionar el Resultado
-	@FXML public RadioButton PositiveRdbtn;
-	@FXML public RadioButton NegativeRdbtn;
-	
-	public void ResultListen(ActionEvent event){
-		this.selectedResult = "";
-		if (PositiveRdbtn.isSelected()){
-			selectedResult += PositiveRdbtn.getText();
-		}
-		if (NegativeRdbtn.isSelected()){
-			selectedResult += NegativeRdbtn.getText();
-		}
-	}
-		
-	//Obtener resultados
-	public void IdListen(ActionEvent event) {
-		this.selectedIdType = IdType.getValue();
-	}
-	public void DateListen(ActionEvent event) {
-		this.selectedExamDate = examDate.getValue().toString();
-	}
-		
-	// Imprimir resultados al apretar el botón Send
-	public void SendAction(ActionEvent event) {
-		
-		selectedExamDate = examDate.getValue().toString();
-		selectedTestId = testIdentifier.getText();
-		selectedPatientIdentifier = patientIdentifier.getText();
-		selectedComments = comments.getText();
-		
-		System.out.println(this.selectedTestId);
-		System.out.println(this.selectedStatus);
-		System.out.println(this.selectedSampleType);
-		System.out.println(this.selectedIdType);
-		System.out.println(this.selectedPatientIdentifier);
-		System.out.println(this.selectedExamDate);
-		System.out.println(this.selectedResult);
-		System.out.println(this.selectedComments);
-	}
+public class Ejemplo  extends Application {
+
+	private Stage stage;
+  private DatePicker checkInDatePicker;
+  private final String pattern = "dd-MM-yyyy";
+
+  public static void main(String[] args) {
+      Locale.setDefault(Locale.US);
+      launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) {
+      this.stage = stage;
+      stage.setTitle("DatePickerSample");
+      initUI();
+      stage.show();
+  }
+
+  private void initUI() {
+      VBox vbox = new VBox(20);
+      vbox.setStyle("-fx-padding: 10;");
+      Scene scene = new Scene(vbox, 400, 400);
+      stage.setScene(scene);
+      checkInDatePicker = new DatePicker();
+      checkInDatePicker.setValue(LocalDate.now());
+      StringConverter converter = new StringConverter<LocalDate>() {
+          DateTimeFormatter dateFormatter = 
+              DateTimeFormatter.ofPattern(pattern);
+          @Override
+          public String toString(LocalDate date) {
+              if (date != null) {
+                  return dateFormatter.format(date);
+              } else {
+                  return "";
+              }
+          }
+          @Override
+          public LocalDate fromString(String string) {
+              if (string != null && !string.isEmpty()) {
+                  return LocalDate.parse(string, dateFormatter);
+              } else {
+                  return null;
+              }
+          }
+      };             
+      checkInDatePicker.setConverter(converter);
+      checkInDatePicker.setPromptText(pattern.toLowerCase());
+      GridPane gridPane = new GridPane();
+      gridPane.setHgap(10);
+      gridPane.setVgap(10);
+      Label checkInlabel = new Label("Fecha de toma de muestra:");
+      gridPane.add(checkInlabel, 0, 0);
+      GridPane.setHalignment(checkInlabel, HPos.LEFT);
+      gridPane.add(checkInDatePicker, 0, 1);
+      vbox.getChildren().add(gridPane);
+      checkInDatePicker.requestFocus();
+      
+  }
 }
